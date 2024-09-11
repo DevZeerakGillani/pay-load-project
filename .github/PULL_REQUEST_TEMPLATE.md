8856c3f49ab52a89fabbabfef8690c11a36ad7a4
DevZeerakGillani <ZeerakGillani@outlook.com>
2024-09-11 08:47:19 -0700

    .
    Payload is the open-source, fullstack Next.js framework, giving you instant backend superpowers. Get a full TypeScript backend and admin panel instantly. Use Payload as a headless CMS or for building powerful applications.

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,10 @@
+<!--
+
+For external contributors, please include:
+
+- A summary of the pull request and any related issues it fixes.
+- Reasoning for the changes made or any additional context that may be useful.
+
+Ensure you have read and understand the [CONTRIBUTING.md](https://github.com/payloadcms/payload/blob/main/CONTRIBUTING.md) document in this repository.
+
+ -->
---
+++ test/fields/collections/Group/index.ts
@@ -0,0 +1,233 @@
+import type { CollectionConfig } from '../../../../packages/payload/src/collections/config/types'
+
+import { groupFieldsSlug } from '../../slugs'
+
+export const groupDefaultValue = 'set from parent'
+export const groupDefaultChild = 'child takes priority'
+
+const GroupFields: CollectionConfig = {
+  slug: groupFieldsSlug,
+  versions: true,
+  fields: [
+    {
+      label: 'Group Field',
+      name: 'group',
+      type: 'group',
+      defaultValue: {
+        defaultParent: groupDefaultValue,
+      },
+      admin: {
+        description: 'This is a group.',
+      },
+      fields: [
+        {
+          name: 'text',
+          type: 'text',
+          required: true,
+          defaultValue: groupDefaultValue,
+        },
+        {
+          name: 'defaultParent',
+          type: 'text',
+          defaultValue: groupDefaultChild,
+        },
+        {
+          name: 'defaultChild',
+          type: 'text',
+          defaultValue: groupDefaultChild,
+        },
+        {
+          name: 'subGroup',
+          type: 'group',
+          fields: [
+            {
+              name: 'textWithinGroup',
+              type: 'text',
+            },
+            {
+              name: 'arrayWithinGroup',
+              type: 'array',
+              fields: [
+                {
+                  name: 'textWithinArray',
+                  type: 'text',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'arrayOfGroups',
+      type: 'array',
+      defaultValue: [
+        {
+          groupItem: {
+            text: 'Hello world',
+          },
+        },
+      ],
+      fields: [
+        {
+          name: 'groupItem',
+          type: 'group',
+          fields: [{ name: 'text', type: 'text' }],
+        },
+      ],
+    },
+    {
+      name: 'potentiallyEmptyGroup',
+      type: 'group',
+      fields: [
+        {
+          name: 'text',
+          type: 'text',
+        },
+      ],
+    },
+    {
+      type: 'row',
+      fields: [
+        {
+          name: 'groupInRow',
+          type: 'group',
+          fields: [
+            {
+              name: 'field',
+              type: 'text',
+            },
+            {
+              name: 'secondField',
+              type: 'text',
+            },
+            {
+              name: 'thirdField',
+              type: 'text',
+            },
+          ],
+        },
+        {
+          name: 'secondGroupInRow',
+          type: 'group',
+          fields: [
+            {
+              name: 'field',
+              type: 'text',
+            },
+            {
+              name: 'nestedGroup',
+              type: 'group',
+              fields: [
+                {
+                  name: 'nestedField',
+                  type: 'text',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          name: 'groups',
+          label: 'Groups in tabs',
+          fields: [
+            {
+              type: 'row',
+              fields: [
+                {
+                  name: 'groupInRow',
+                  type: 'group',
+                  fields: [
+                    {
+                      name: 'field',
+                      type: 'text',
+                    },
+                    {
+                      name: 'secondField',
+                      type: 'text',
+                    },
+                    {
+                      name: 'thirdField',
+                      type: 'text',
+                    },
+                  ],
+                },
+                {
+                  name: 'secondGroupInRow',
+                  type: 'group',
+                  fields: [
+                    {
+                      name: 'field',
+                      type: 'text',
+                    },
+                    {
+                      name: 'nestedGroup',
+                      type: 'group',
+                      fields: [
+                        {
+                          name: 'nestedField',
+                          type: 'text',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'camelCaseGroup',
+      type: 'group',
+      fields: [
+        {
+          name: 'array',
+          type: 'array',
+          fields: [
+            {
+              type: 'text',
+              name: 'text',
+              localized: true,
+            },
+            {
+              type: 'array',
+              name: 'array',
+              fields: [
+                {
+                  type: 'text',
+                  name: 'text',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'nesGroup',
+          type: 'group',
+          fields: [
+            {
+              type: 'array',
+              name: 'arr',
+              fields: [
+                {
+                  type: 'text',
+                  name: 'text',
+                  localized: true,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+export default GroupFields
